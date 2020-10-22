d4913623444421a6056e98fa41915f3dd07bc51f
adesimt <adesimt@dunwoody.edu>
2020-10-22 12:59:24 -0500

    Assignment 3 initiated

--- Program.cs
+++ Program.cs
@@ -1,11 +1,12 @@
 ﻿using System;
+
 /**
 Name: 
 Date of completion:
-
 Please be sure to comment your code - provide a comment for each structure use in 
 program.
  */
+
 namespace assignment_three
 {
     class Program
